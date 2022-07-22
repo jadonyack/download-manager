bb24c511498098c4bcae43110050294c4b33dcf8
Jadon Yack <jadon.yack@gmail.com>
2022-07-21 20:22:24 -0400

    Generalize username. Add shebang.

--- download_manager.py
+++ download_manager.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # This is a Python script that automates the management of the Downloads folder 
 # for an organized experience. It will sort images, videos, documents, and ISO
 # files on download.
@@ -6,9 +7,10 @@
 
 import os
 
-src_dir = "/home/jyack/Downloads"
-img_dst = "/home/jyack/Pictures/DownloadedImages"
-vid_dst = "/home/jyack/Videos"
-doc_dst = "/home/jyack/Documents"
-iso_dst = "/home/jyack/ISOs"
+user = os.getlogin()
+src_dir = "/home/" + user + "/Downloads"
+img_dst = "/home/" + user + "/Pictures/DownloadedImages"
+vid_dst = "/home/" + user + "/Videos"
+doc_dst = "/home/" + user + "/Documents"
+iso_dst = "/home/" + user + "/ISOs"
 
