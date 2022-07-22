46608ba1bf4d067e151b43aab9c65119afd4d988
Jadon Yack <jadon.yack@gmail.com>
2022-07-21 20:01:41 -0400

    Initial commit.

---
+++ download_manager.py
@@ -0,0 +1,14 @@
+# This is a Python script that automates the management of the Downloads folder 
+# for an organized experience. It will sort images, videos, documents, and ISO
+# files on download.
+#
+# Written by Jadon Yack (jyack)
+
+import os
+
+src_dir = "/home/jyack/Downloads"
+img_dst = "/home/jyack/Pictures/DownloadedImages"
+vid_dst = "/home/jyack/Videos"
+doc_dst = "/home/jyack/Documents"
+iso_dst = "/home/jyack/ISOs"
+
